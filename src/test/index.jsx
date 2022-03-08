40cba9bec42f39604989db8d13a33d3c293b756f
wms001 <1532601761@qq.com>
2022-03-08 21:58:13 +0800

    dev

---
+++ src/test/index.jsx
@@ -0,0 +1,13 @@
+/*
+ * @Descripttion:
+ * @version: X3版本
+ * @Author: 吴毛三
+ * @Date: 2022-03-08 21:54:32
+ * @LastEditors: 吴毛三
+ * @LastEditTime: 2022-03-08 21:54:33
+ */
+import React from "react";
+
+export const Index = () => {
+  return <div>index</div>;
+};
