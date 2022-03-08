a1cecd73c36f9a364331bab8427369eaa8a2d61b
wms001 <1532601761@qq.com>
2022-03-08 22:58:10 +0800

    34234324

--- src/test/index.jsx
+++ src/test/index.jsx
@@ -4,10 +4,10 @@
  * @Author: 吴毛三
  * @Date: 2022-03-08 21:54:32
  * @LastEditors: 吴毛三
- * @LastEditTime: 2022-03-08 21:54:33
+ * @LastEditTime: 2022-03-08 22:48:18
  */
 import React from "react";
 
 export const Index = () => {
-  return <div>index</div>;
+  return <div>3242342</div>;
 };
