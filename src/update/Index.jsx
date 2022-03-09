a667855325c200c54da3eb7a31ebd92e97fb1066
wms001 <1532601761@qq.com>
2022-03-09 20:13:23 +0800

    update

---
+++ src/update/Index.jsx
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion:
+ * @version: X3版本
+ * @Author: 吴毛三
+ * @Date: 2022-03-09 20:12:40
+ * @LastEditors: 吴毛三
+ * @LastEditTime: 2022-03-09 20:12:55
+ */
+import React from "react";
+
+const Index = () => {
+  return <div>Index</div>;
+};
+
+export default Index;
