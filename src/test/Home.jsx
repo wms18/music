05b3f1b33d0135cd68541f5ab38604b851ea8296
wms001 <1532601761@qq.com>
2022-03-09 20:11:23 +0800

    update

--- src/test/Home.jsx
+++ src/test/Home.jsx
@@ -4,12 +4,12 @@
  * @Author: 吴毛三
  * @Date: 2022-03-08 22:51:12
  * @LastEditors: 吴毛三
- * @LastEditTime: 2022-03-08 23:00:32
+ * @LastEditTime: 2022-03-09 20:10:49
  */
 import React from "react";
 
 const Home = () => {
-  return <div>59843058309</div>;
+  return <div>59843058</div>;
 };
 
 export default Home;
