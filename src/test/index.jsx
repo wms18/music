88b737dbe0d3808029396d25941d09871b689ee6
wms001 <1532601761@qq.com>
2022-03-10 20:26:29 +0800

    update

--- src/test/index.jsx
+++ src/test/index.jsx
@@ -4,10 +4,10 @@
  * @Author: 吴毛三
  * @Date: 2022-03-08 21:54:32
  * @LastEditors: 吴毛三
- * @LastEditTime: 2022-03-08 22:48:18
+ * @LastEditTime: 2022-03-10 20:21:50
  */
 import React from "react";
 
 export const Index = () => {
-  return <div>3242342</div>;
+  return <div>gjkghj</div>;
 };
--- src/update/Index.jsx
+++ src/update/Index.jsx
@@ -4,12 +4,12 @@
  * @Author: 吴毛三
  * @Date: 2022-03-09 20:12:40
  * @LastEditors: 吴毛三
- * @LastEditTime: 2022-03-09 20:12:55
+ * @LastEditTime: 2022-03-10 20:24:25
  */
 import React from "react";
 
 const Index = () => {
-  return <div>Index</div>;
+  return <div>update</div>;
 };
 
 export default Index;
