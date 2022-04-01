ea0ff609a333efd7c0c1ffae512cad2be6a96621
wms001 <1532601761@qq.com>
2022-04-01 18:33:01 +0800

    1234

--- src/test/Home.jsx
+++ src/test/Home.jsx
@@ -4,14 +4,17 @@
  * @Author: 吴毛三
  * @Date: 2022-03-08 22:51:12
  * @LastEditors: 吴毛三
- * @LastEditTime: 2022-03-22 11:42:21
+ * @LastEditTime: 2022-04-01 18:32:07
  */
 import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <div>home1 </div>
+      <div>
+        <div>dfhksalhjfkldsaj</div>
+      </div>
+      <div>sdfhkjsahdfjksaldhfkjsdh</div>
     </div>
   );
 };
---
+++ src/test/Index.jsx
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion:
+ * @version: X3版本
+ * @Author: 吴毛三
+ * @Date: 2022-04-01 18:32:27
+ * @LastEditors: 吴毛三
+ * @LastEditTime: 2022-04-01 18:32:28
+ */
+import React from "react";
+
+const Index = () => {
+  return <div>Index</div>;
+};
+
+export default Index;
