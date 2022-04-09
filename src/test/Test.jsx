962462efee701c0cf67761ff75ccfa47e63b64ec
wms001 <1532601761@qq.com>
2022-04-09 19:26:43 +0800

    ggg

--- src/test/Test.jsx
+++ src/test/Test.jsx
@@ -4,14 +4,11 @@
  * @Author: 吴毛三
  * @Date: 2022-04-01 18:35:24
  * @LastEditors: 吴毛三
- * @LastEditTime: 2022-04-01 18:35:25
+ * @LastEditTime: 2022-04-09 19:23:45
  */
-import React from 'react'
+import React from "react";
 
 const Test = () => {
-  return (
-    <div>Test</div>
-  )
-}
-
-export default Test
+  return <div>Test</div>;
+};
+export default Test;
